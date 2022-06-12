1b03131bafadc74f88a34b77e580f35d0680e4e1
pawjam <jamro.pawel@gmail.com>
2022-06-12 12:46:09 +0200

    Click button implemented

--- src/main/java/pl/lublin/wsei/java/cwiczenia/lab2fx/lab2javafx/HelloController.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/lab2fx/lab2javafx/HelloController.java
@@ -1,14 +1,16 @@
 package pl.lublin.wsei.java.cwiczenia.lab2fx.lab2javafx;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public Button btnMain;
+    public int clicCounter;
+
+    public void onBtnAction(ActionEvent actionEvent) {
+        System.out.println("Kliknięcie nr: "+ ++clicCounter);
     }
 }
